dd297583887d4b821f6c4cb18c22bacdb9e63c06
VolodiaHunkalo <graperz123@gmail.com>
2024-06-05 10:56:06 +0300

    v0.1.12 ref: add button to public import

--- lib/webitel_portal_sdk.dart
+++ lib/webitel_portal_sdk.dart
@@ -1,9 +1,12 @@
 library webitel_portal_sdk;
 
+export 'src/domain/entities/button.dart';
+export 'src/domain/entities/button_type.dart';
 export 'src/domain/entities/call_error.dart';
 export 'src/domain/entities/channel_status.dart';
 export 'src/domain/entities/connect_status.dart';
 export 'src/domain/entities/dialog.dart';
+export 'src/domain/entities/keyboard.dart';
 export 'src/domain/entities/logger_level.dart';
 export 'src/domain/entities/portal_chat_member.dart';
 export 'src/domain/entities/portal_client.dart';
